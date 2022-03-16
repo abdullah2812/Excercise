e4403ddcc2ad969369bdca3134d9397aae8380a8
Abdullah <iriandaabdullah546@gmail.com>
2022-03-16 08:33:48 +0700

    Initial Commit

---
+++ Exercise1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exercise1
+//
+//  Created by Irianda on 16/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
