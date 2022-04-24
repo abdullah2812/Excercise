64857662a89efd55c28f6c2b8830076311e28ff5
Abdullah <iriandaabdullah546@gmail.com>
2022-04-24 13:49:01 +0700

    Tugas3

--- Exercise2/ViewController.swift
+++ Exercise2/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btn: UILabel!
+    
+    @IBOutlet weak var btnlogin: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        btn.layer.cornerRadius = 10
+        btn.clipsToBounds = true
+        btnlogin.layer.cornerRadius = 10
+        btnlogin.clipsToBounds = true
     }
-
+    
+    //untuk light Bar Style IOS
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setNeedsStatusBarAppearanceUpdate()
+    }
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        .lightContent
+    }
 
 }
 
